056a4bb363a513a7d6e484b890bbf4eefaa5748e
am4352 <anuj69937@gmail.com>
2024-08-21 22:28:33 +0530

    datatype summary

---
+++ 1-10 lectures/lecture7.js
@@ -0,0 +1,28 @@
+// let str1 = "hello"
+// let str2 = " mishra"
+// let str3 = str1 + str2
+// console.log(str3);
+// console.log("1" + "2");
+// console.log("1" + 2);
+// console.log(1 + 2+"2"); 
+// console.log(+true)
+// console.log(-true)
+// console.log(+" ");
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+let gamecounter = 100
+++gamecounter;
+console.log(gamecounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 1-10 lectures/lecture9.js
@@ -0,0 +1,30 @@
+// const id = Symbol('123')
+// const anotherid = Symbol('123')
+// console.log(id === anotherid);
+// const bignumber = 5665584225658255852n
+// console.log(typeof(bignumber));
+const heroes = ["anuj", "mishra", "hello"]
+ let myobj = {
+    name: "mom",
+    age : 25
+}
+const myfunction = function ()
+{
+    console.log("hello world");   
+}
+console.log(typeof myfunction);
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
