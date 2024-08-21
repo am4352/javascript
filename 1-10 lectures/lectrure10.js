85155f3c048b82c3ed2cd0d65462ffedc8694253
am4352 <anuj69937@gmail.com>
2024-08-21 23:02:09 +0530

    stacks and heaps

---
+++ 1-10 lectures/lectrure10.js
@@ -0,0 +1,16 @@
+let myyoutubename = "anujmishra"
+let anothername = myyoutubename
+anothername = "helloname"
+console.log(anothername);
+console.log(myyoutubename);
+
+let userone = {
+    email: "anuj@google.com",
+    upi : "anuj@ybl",
+}
+let usertwo = userone;
+usertwo.email = "anujmishra@google.com"
+
+console.log(userone.email)
+console.log(usertwo.email);
+
