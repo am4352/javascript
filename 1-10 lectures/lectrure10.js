34fa60eb5a74c8c1c6f6c592ebac579893e50edb
am4352 <anuj69937@gmail.com>
2024-08-22 00:13:39 +0530

    hello

--- 1-10 lectures/lectrure10.js
+++ 1-10 lectures/lectrure10.js
@@ -6,11 +6,11 @@ console.log(myyoutubename);
 
 let userone = {
     email: "anuj@google.com",
-    upi : "anuj@ybl",
+    upi : "anuj@ybl"
 }
 let usertwo = userone;
 usertwo.email = "anujmishra@google.com"
 
-console.log(userone.email)
+console.log(userone.email);
 console.log(usertwo.email);
 
