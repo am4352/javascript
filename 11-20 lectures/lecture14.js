d3ecaf3cf5c2eba7a77360eab81ffb07d822e7e6
anuj69937 <anuj69937@gmail.com>
2024-08-27 21:51:22 +0530

    splice and slice

---
+++ 11-20 lectures/lecture14.js
@@ -0,0 +1,23 @@
+const array = [1, 2, 3, 5,6,8, "anuj"]
+// console.log(array);
+// console.log(array[1])
+// array.unshift(9)
+// console.log(array);
+const newarray = array.join()// join converts array into string 
+console.log(array);
+console.log(newarray);// 
+console.log("A ", array);
+const myn1 = array.slice(1, 3)
+console.log("b", myn1);
+const myn2 = array.splice(1, 3)
+console.log(array);
+console.log(myn2);
+
+
+
+
+
+
+
+
+
