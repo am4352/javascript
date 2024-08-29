a78d13f4707d935299cd459dc6b20caaebbebcb0
anuj69937 <anuj69937@gmail.com>
2024-08-30 00:27:33 +0530

    json api

---
+++ 11-20 lectures/lecture18.js
@@ -0,0 +1,23 @@
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseinstructor: "anuj"
+}
+
+// course.courseinstructor
+const { courseinstructor : instructor } = course// values can be directly used by using const
+// console.log(courseinstructor); //
+console.log(instructor);
+// {
+//     "name": "anuj",
+//     "coursename": "js in hindi",    // sample json file
+//     "price" : 2000
+// }
+
+
+
+
+
+
+
+
