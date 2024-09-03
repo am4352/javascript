9df0c536151943b906947214a69600015a822cb6
anuj69937 <anuj69937@gmail.com>
2024-09-03 11:01:32 +0530

    scopes

---
+++ 21-30lectures/lecture21.js
@@ -0,0 +1,21 @@
+var c = 300
+if (true) { // block scope 
+let a = 10;
+const b = 20;
+    var c = 30;
+    console.log("inner :" , a);
+    
+}
+//outside is global scope
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+// console.log(a);
+// console.log(b);
+console.log(c);// c is accesible outside scope bacause we are using var 
+
+
+
+
+
