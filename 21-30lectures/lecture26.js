5cb0af18a813139de1e8509bc50685b5b268d272
am4352 <am4352@srmist.edu.in>
2024-09-08 16:25:20 +0530

    call back function

---
+++ 21-30lectures/lecture26.js
@@ -0,0 +1,40 @@
+const coding = [1, 8, 9, 5, 6, 7];
+
+
+// coding.forEach(function (val)
+// {
+//     console.log(val);
+    
+// })
+coding.forEach((item , index, arr) =>  //callback function
+{
+    console.log(item, index , arr); // prints item, index no. and whole array
+    
+})  
+
+
+/*const mycoding = [
+     {
+    languagename : "javascript",
+    filename : "js"
+    },
+    {
+        languagename: "python",
+        filename:"py"
+    },
+    {
+        languagename: "golang",
+        filename:"go"
+    },
+     
+]*/
+mycoding.forEach((item) => ///foreach    call back function
+{
+    console.log(item.filename);
+    
+})
+mycoding.forEach((item) =>
+{
+    console.log(item.languagename);
+    
+})
