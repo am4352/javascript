7a4c2123c8af5cd2e52f864d9ef9df4c54f0511d
am4352 <am4352@srmist.edu.in>
2024-09-26 19:29:51 +0530

    async js

---
+++ async.js
@@ -0,0 +1,23 @@
+/* default  javascript is synchronous 
+
+execution context
+
+blocking code vs non blocking code
+blocking code block the flow of the code 
+read file sync
+
+
+
+
+
+
+
+
+-------------------------------------------
+
+non blocking code does not block the flow of code
+read file async 
+
+
+task queue 
+*/
